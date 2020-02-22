78dd932bae28911928b89cdf20cf464ce982baa1
Daisuke Nakano <mail@nakanod.net>
2020-02-22 20:03:55 +0900

    Modify to use Jinja2 template

--- google_dns.py
+++ google_dns.py
@@ -4,6 +4,34 @@ import sys
 
 import dns.zone
 import dns.rdatatype
+from jinja2 import Environment, FileSystemLoader
+
+
+class GoogleDNSRecord(object):
+    def __init__(self, origin, name, rdataset, zone_defined_name):
+        self.rdata = []
+        self.rdtype = dns.rdatatype.to_text(rdataset.rdtype)
+        self.ttl = int(rdataset.ttl)
+        self.managed_zone = 'google_dns_managed_zone.{}.name'.format(zone_defined_name)
+        origin_hyphen = origin[:-1].replace('.', '-')
+        sub_domain = str(name)
+        naked_domain = 'google_dns_managed_zone.{}.dns_name'.format(zone_defined_name)
+        if sub_domain == '@':
+            self.name = naked_domain
+            self.defined_name = '{}-{}'.format(self.rdtype.lower(), origin_hyphen)
+        else:
+            self.name = '"{}.${{{}}}"'.format(sub_domain, naked_domain)
+            self.defined_name = '{}-{}-{}'.format(self.rdtype.lower(), sub_domain.replace('.', '-'), origin_hyphen)
+        self.rdata = ['"{}"'.format(rdata.to_text().replace('"', '\\"')) for rdata in rdataset]
+
+
+class GoogleDNSZone(object):
+    def __init__(self, filename):
+        self.rdataset = []
+        zone = dns.zone.from_file(filename)
+        self.origin = str(zone.origin)
+        self.defined_name = 'zone-{}'.format(self.origin[:-1].replace('.', '-'))
+        self.rdataset = [GoogleDNSRecord(self.origin, name, rdataset, self.defined_name) for name, node in zone.nodes.items() for rdataset in node.rdatasets]
 
 
 def help():
@@ -17,50 +45,13 @@ def main():
         return
 
     zonefile = sys.argv[1]
-    zone = dns.zone.from_file(zonefile)
-    managed_zone_dns_name = str(zone.origin)
-    origin_hyphen = managed_zone_dns_name[:-1].replace(".", "-")
-    managed_zone_variable = 'zone-{}'.format(origin_hyphen)
+    zone = GoogleDNSZone(zonefile)
+    loader = FileSystemLoader("templates")
+    env = Environment(loader=loader)
+    template = env.get_template('google_dns.tf.j2')
+    render = template.render(zone=zone)
 
-    managed_zone_string = 'resource "google_dns_managed_zone" "{}" {{\n'.format(managed_zone_variable)
-    managed_zone_string += '  name = "{}"\n'.format(managed_zone_variable)
-    managed_zone_string += '  dns_name = "{}"\n'.format(managed_zone_dns_name)
-    managed_zone_string += '  visibility = "public"\n'
-    managed_zone_string += '}\n'
-    print(managed_zone_string)
-
-    naked_domain = "google_dns_managed_zone.{}.dns_name".format(managed_zone_variable)
-
-    for name, node in zone.nodes.items():
-        for rdataset in node.rdatasets:
-            rdtype = dns.rdatatype.to_text(rdataset.rdtype)
-            ttl = rdataset.ttl
-            sub_domain = str(name)
-            rrdatas = ''
-
-            if rdtype == 'SOA' or rdtype == 'NS':
-                continue
-
-            for rdata in rdataset:
-                rrdatas += '"{}",'.format(rdata.to_text().replace('"', '\\"'))
-
-            if sub_domain == '@':
-                name_string = '{}'.format(naked_domain)
-                record_set_variable = '{}-{}'.format(rdtype.lower(), origin_hyphen)
-                pass
-            else:
-                name_string = '"{}.${{{}}}"'.format(sub_domain, naked_domain)
-                record_set_variable = '{}-{}-{}'.format(rdtype.lower(), sub_domain.replace(".", "-"), origin_hyphen)
-                pass
-
-            record_set_string = 'resource "google_dns_record_set" "{}" {{\n'.format(record_set_variable)
-            record_set_string += '  type = "{}"\n'.format(rdtype)
-            record_set_string += '  ttl = {}\n'.format(ttl)
-            record_set_string += '  name = {}\n'.format(name_string)
-            record_set_string += '  managed_zone = google_dns_managed_zone.{}.name\n'.format(managed_zone_variable)
-            record_set_string += '  rrdatas = [{}]\n'.format(rrdatas)
-            record_set_string += '}\n'
-            print(record_set_string)
+    print(render)
 
 
 if __name__ == '__main__':
--- route53.py
+++ route53.py
@@ -4,6 +4,37 @@ import sys
 
 import dns.zone
 import dns.rdatatype
+from jinja2 import Environment, FileSystemLoader
+
+
+class Route53Record(object):
+    def __init__(self, origin, name, rdataset, zone_defined_name):
+        self.rdata = []
+        self.rdtype = dns.rdatatype.to_text(rdataset.rdtype)
+        self.ttl = int(rdataset.ttl)
+        self.zone_id = 'aws_route53_zone.{}.zone_id'.format(zone_defined_name)
+        origin_hyphen = origin[:-1].replace('.', '-')
+        sub_domain = str(name)
+        naked_domain = 'aws_route53_zone.{}.name'.format(zone_defined_name)
+        if sub_domain == '@':
+            self.name = naked_domain
+            self.defined_name = '{}-{}'.format(self.rdtype.lower(), origin_hyphen)
+        else:
+            self.name = '"{}.${{{}}}"'.format(sub_domain, naked_domain)
+            self.defined_name = '{}-{}-{}'.format(self.rdtype.lower(), sub_domain.replace('.', '-'), origin_hyphen)
+        if self.rdtype == 'TXT':
+            self.rdata = ['{}'.format(rdata.to_text()) for rdata in rdataset]
+        else:
+            self.rdata = ['"{}"'.format(rdata.to_text().replace('"', '\\"')) for rdata in rdataset]
+
+
+class Route53Zone(object):
+    def __init__(self, filename):
+        self.rdataset = []
+        zone = dns.zone.from_file(filename)
+        self.origin = str(zone.origin)
+        self.defined_name = 'zone-{}'.format(self.origin[:-1].replace('.', '-'))
+        self.rdataset = [Route53Record(self.origin, name, rdataset, self.defined_name) for name, node in zone.nodes.items() for rdataset in node.rdatasets]
 
 
 def help():
@@ -17,52 +48,13 @@ def main():
         return
 
     zonefile = sys.argv[1]
-    zone = dns.zone.from_file(zonefile)
-    managed_zone_dns_name = str(zone.origin)
-    origin_hyphen = managed_zone_dns_name[:-1].replace(".", "-")
-    managed_zone_variable = 'zone-{}'.format(origin_hyphen)
+    zone = Route53Zone(zonefile)
+    loader = FileSystemLoader("templates")
+    env = Environment(loader=loader)
+    template = env.get_template('route53.tf.j2')
+    render = template.render(zone=zone)
 
-    managed_zone_string = 'resource "aws_route53_zone" "{}" {{\n'.format(managed_zone_variable)
-    managed_zone_string += '  name = "{}"\n'.format(managed_zone_dns_name)
-    managed_zone_string += '}\n'
-    print(managed_zone_string)
-
-    naked_domain = "aws_route53_zone.{}.name".format(managed_zone_variable)
-
-    for name, node in zone.nodes.items():
-        for rdataset in node.rdatasets:
-            rdtype = dns.rdatatype.to_text(rdataset.rdtype)
-            ttl = rdataset.ttl
-            sub_domain = str(name)
-            rrdatas = ''
-
-            if rdtype == 'SOA' or rdtype == 'NS':
-                continue
-
-            if rdtype == 'TXT':
-                for rdata in rdataset:
-                    rrdatas += '{},'.format(rdata.to_text())
-            else:
-                for rdata in rdataset:
-                    rrdatas += '"{}",'.format(rdata.to_text().replace('"', '\\"'))
-
-            if sub_domain == '@':
-                name_string = '{}'.format(naked_domain)
-                record_set_variable = '{}-{}'.format(rdtype.lower(), origin_hyphen)
-                pass
-            else:
-                name_string = '"{}.${{{}}}"'.format(sub_domain, naked_domain)
-                record_set_variable = '{}-{}-{}'.format(rdtype.lower(), sub_domain.replace(".", "-"), origin_hyphen)
-                pass
-
-            record_set_string = 'resource "aws_route53_record" "{}" {{\n'.format(record_set_variable)
-            record_set_string += '  type = "{}"\n'.format(rdtype)
-            record_set_string += '  ttl = {}\n'.format(ttl)
-            record_set_string += '  name = {}\n'.format(name_string)
-            record_set_string += '  zone_id = aws_route53_zone.{}.zone_id\n'.format(managed_zone_variable)
-            record_set_string += '  records = [{}]\n'.format(rrdatas)
-            record_set_string += '}\n'
-            print(record_set_string)
+    print(render)
 
 
 if __name__ == '__main__':
